ea245e49e289be89b09c6ff6f1727bc87a641674
AndrewSpencer23 <alspencer@mavs.coloradomesa.edu>
2023-09-06 18:01:09 -0700

    ASCII Lab v2 (090623)

--- Labs/asciiLab.cpp
+++ Labs/asciiLab.cpp
@@ -2,8 +2,8 @@
     StdIO Lab
     ASCII Art - using literals and variables
     
-    Updated By: <Your name> #FIXME1
-    Date: ... #FIXME2
+    Updated By: Andrew Spencer #FIXME #fixed#
+    Date: 09/06/23 #FIXME2 #fixed#
     
     This program produces an ASCII art on the console.
 
@@ -19,11 +19,23 @@ using namespace std; //resolve cout, cin, and endl names
  
 //main entry point of the program
 int main()
-{
-    //FIXME3: declare a variable to store name
-    //FIXME4: prompt user to enter their name and store the value in variable
-    
-    //FIXME5: greet the name using the variable as the following output
+{   
+    //FIXME3: declare a variable to store name #fixed#
+
+    string firstName;
+    string lastName;
+
+    //FIXME4: prompt user to enter their name and store the value in variable\ #fixed#
+
+    cout << "Enter your first name: ";
+    cin >> firstName;
+
+    cout << "Enter your last name: ";
+    cin >> lastName;
+
+    cout << "Welcome to the program " << firstName << " " << lastName << ", it is nice to meet you!" << endl;
+
+    //FIXME5: greet the name using the variable as the following output #fixed#
     //must output: Nice meeting you, <name>!
 
     cout << "Hope you like my Tom & Jerry ASCII art...\n\n";
@@ -31,7 +43,9 @@ int main()
     string line1 = "  |\\_/|   **********************    (\\_/)\n";
     cout << line1;
 
-    //FIXME6: use variable to print the second line line of the graphic
+    string line2 = "/ @ @ \\   *      ASCII Lab      *   (='.'=)\n";
+    cout << line2;
+    //FIXME6: use variable to print the second line line of the graphic #fixed#
     //FIXME7: print the third line of the graphics
     //FIXME8: use variable to print the fourth line
     //FIXME9: use variable to print the fifth line
