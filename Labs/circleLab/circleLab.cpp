85e1720ee8de84ae8f1bf1756e1d2342c8e92002
AndrewSpencer23 <alspencer@mavs.coloradomesa.edu>
2023-09-19 19:19:02 -0700

    CircleLab First Commit

---
+++ Labs/circleLab/circleLab.cpp
@@ -0,0 +1,36 @@
+// Andrew Spencer
+
+// 09/17/23
+
+// Area and circumference of circle given radius
+
+//Included libraries:
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <string>
+
+using namespace std; // resolve cout, cin, and endl names
+
+string promptName();
+void greetUser(string);
+void getVaulues(float&);
+
+
+int main() {
+    string fullName;
+    float radius;
+    
+
+
+
+}
+
+string promptName() {
+    
+                                                                                 
+}
+
+void greetUser(string){
+
+}
