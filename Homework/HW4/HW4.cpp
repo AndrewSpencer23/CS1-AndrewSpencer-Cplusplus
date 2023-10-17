c2b34569e9b0e54fc1c590a991320a5958e4cd73
AndrewSpencer23 <alspencer@mavs.coloradomesa.edu>
2023-10-16 20:02:40 -0700

    Conditionals first commit

---
+++ Homework/HW4/HW4.cpp
@@ -0,0 +1,28 @@
+// Andrew Spencer
+
+// HW4 ; Sum functions with conditionals
+
+// 10-10-23
+
+// 1. Define a function that takes 5 numbers, calculates and returns the sum of the numbers. (5 points)
+// 2. Define a function that takes 5 numbers, calculates and returns the product of the numbers. (5 points)
+// 3. Define a function that takes 5 numbers, calculates and returns the average of the numbers. Must call the function you defined earlier to find the sum of the five numbers. (5 points)
+// 4. Define a function that takes 5 numbers, finds and returns the largest value among the numbers. Must use your own conditional statements and NOT use any built-in function. (5 points)
+// 5. Define a function that takes 5 numbers, finds and returns the smallest value among the numbers. Must use your own conditional statements and NOT use any built-in function. (10 points)
+// 6. Define a function that takes five numbers and returns if the floor of the sum of those numbers is even, odd or zero. (5 points)
+// 7. At least one function must be templated to work on various types (5 points).
+// 8. Greet the user by their name and prompt them to enter 5 numbers. (5 points)
+// 9. Display menu options for each function and call the specific function based on the user's choice to calculate the result using switch statement and display the result with proper descriptions. (5 points)
+// 10. Define a function called test (5*5=25 points)
+// 11. For each of the functions you defined above write at least 3 automated test cases using assert statements to automatically test and verify that the functions provide correct results. 
+// 12. Try to come up with corner cases (various data) to confirm the functions still provide the correct results.
+// 13. Call the test function only if the “test” argument is passed to main. (5 points)
+
+#include <iostream>
+#include <cmath>
+#include <cstdio>
+#include <string>
+
+
+
+
