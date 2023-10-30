53b61176f6160871e1cfaa2a624e387878dc936d
AndrewSpencer23 <alspencer@mavs.coloradomesa.edu>
2023-10-30 13:12:04 -0700

    loopsLab Progress

--- Labs/loopsLab/loopsLab.cpp
+++ Labs/loopsLab/loopsLab.cpp
@@ -1,8 +1,8 @@
 /*
     Loops Lab
-    Updated By: FIXME1
-    CSCI 111
-    Date: FIXME2
+    Updated By: Andrew Spencer #fixed#
+    CSCI 111 
+    Date: 10/15/23 #fixed#
 
     Program prints geometric shapes of given height with * using loops
 */
@@ -26,10 +26,19 @@ void printTriangle(int height) {
         row += 1;
         cout << endl;
     }
+    cout << endl;
 }
 
 
 void printFlippedTriangle(int height) {
+    int row = 1;
+    while (row <= height) {
+        for(int col = 5; col>=row; col--)
+            cout << "* ";
+        row += 1;
+        cout << endl;
+    }
+cout << endl;
     /* 
     Implement the function that takes height as an argument
     and prints a triangle with * of given height.
@@ -41,23 +50,32 @@ void printFlippedTriangle(int height) {
     *
     
     */
-    // FIXME3 ...
+    // FIXME3 #fixed#
 }
 
-
+void printSquare(int height) {
+    int row = 1;
+    while(row<=height)  {
+        int col = 5;
+        for(int row = 1; row<=5; row++)
+            cout << "* ";
+        row += 1;
+        cout << endl;
+    }
+cout << endl;
 /*  
-FIXME4
+FIXME4 #fixed#
 Design and implement a function that takes an integer as height and
 prints square of the given height with *.
 Square of height 5, e.g., would look like the following.
-
 *  *  *  *  *  
 *  *  *  *  *   
 *  *  *  *  *   
 *  *  *  *  *   
 *  *  *  *  *   
-
 */
+}
+
 
 // function clears the screen system call
 // NOTE: system call is not a security best practice!
@@ -72,15 +90,21 @@ void clearScreen() {
 
 int main(int argc, char* argv[]) {
     // FIXME5 add a loop to make the program to continue to run until the user wants to quit
+
     // FIXME6 call clearScreen function to clear the screen for each round of the loop
+
+
     int height;
     cout << "Program prints geometric shapes of given height with *\n";
     cout << "Please enter the height of the shape: ";
     cin >> height;
     // call printTriangle function passing user entered height
     printTriangle(height);
+    printFlippedTriangle(height);
+    printSquare(height);
+    
 
-    // FIXME7
+    // FIXME7 #fixed#
     // Call printFlippedTriangle passing proper argument
     // Manually test the function
 
