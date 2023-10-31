629be238781469049f96cd45c53113be26371198
AndrewSpencer23 <alspencer@mavs.coloradomesa.edu>
2023-10-30 21:42:16 -0700

    loopsLab progress

--- Labs/loopsLab/loopsLab.cpp
+++ Labs/loopsLab/loopsLab.cpp
@@ -13,7 +13,6 @@
 
 using namespace std;
 
-
 void printTriangle(int height) {
     //Function takes height as an argument to print the triangle
     //of that height with *
@@ -29,7 +28,6 @@ void printTriangle(int height) {
     cout << endl;
 }
 
-
 void printFlippedTriangle(int height) {
     int row = 1;
     while (row <= height) {
@@ -38,7 +36,7 @@ void printFlippedTriangle(int height) {
         row += 1;
         cout << endl;
     }
-cout << endl;
+    cout << endl;
     /* 
     Implement the function that takes height as an argument
     and prints a triangle with * of given height.
@@ -55,27 +53,26 @@ cout << endl;
 
 void printSquare(int height) {
     int row = 1;
-    while(row<=height)  {
-        int col = 5;
-        for(int row = 1; row<=5; row++)
+    while(row <= height)  {
+        int col = 1;
+        for(int col = 1; col <= 5; col++)
             cout << "* ";
         row += 1;
         cout << endl;
     }
-cout << endl;
-/*  
-FIXME4 #fixed#
-Design and implement a function that takes an integer as height and
-prints square of the given height with *.
-Square of height 5, e.g., would look like the following.
-*  *  *  *  *  
-*  *  *  *  *   
-*  *  *  *  *   
-*  *  *  *  *   
-*  *  *  *  *   
-*/
+    cout << endl;
+    /*  
+    FIXME4 #fixed#
+    Design and implement a function that takes an integer as height and
+    prints square of the given height with *.
+    Square of height 5, e.g., would look like the following.
+    *  *  *  *  *  
+    *  *  *  *  *   
+    *  *  *  *  *   
+    *  *  *  *  *   
+    *  *  *  *  *   
+    */
 }
-
 
 // function clears the screen system call
 // NOTE: system call is not a security best practice!
@@ -90,33 +87,37 @@ void clearScreen() {
 
 int main(int argc, char* argv[]) {
     // FIXME5 add a loop to make the program to continue to run until the user wants to quit
+    char again = 'Y';
 
-    // FIXME6 call clearScreen function to clear the screen for each round of the loop
+    while (again == 'y' || again == 'Y')   {
+        clearScreen();
+        // FIXME6 call clearScreen function to clear the screen for each round of the loop #fixed#
+        int height;
+        cout << "Program prints geometric shapes of given height with *\n";
+        cout << "Please enter the height of the shape: ";
+        cin >> height;
 
+        clearScreen();
+        // call printTriangle function passing user entered height
+        printTriangle(height);
 
-    int height;
-    cout << "Program prints geometric shapes of given height with *\n";
-    cout << "Please enter the height of the shape: ";
-    cin >> height;
-    // call printTriangle function passing user entered height
-    printTriangle(height);
-    printFlippedTriangle(height);
-    printSquare(height);
-    
+        printFlippedTriangle(height);
+        // FIXME7 #fixed#
+        // Call printFlippedTriangle passing proper argument
+        // Manually test the function
 
-    // FIXME7 #fixed#
-    // Call printFlippedTriangle passing proper argument
-    // Manually test the function
+        printSquare(height);
+        // FIXME8 #fixed#
+        // Call the function defined in FIXME4 passing proper argument
+        // Manually test the function
 
-    // FIXME6
-    // Call the function defined in FIXME4 passing proper argument
-    // Manually test the function
+        // FIXME9 #fixed#
+        // prompt user to enter y/Y to continue anything else to quit
 
-    // FIXME9
-    // prompt user to enter y/Y to continue anything else to quit
-
-    // FIXME10
-    // Use conditional statements to break the loop or continue the loop
-
-    return 0;
+        // FIXME10 #fixed#
+        // Use conditional statements to break the loop or continue the loop
+        cout << "Go Again? Y/N: ";
+        cin >> again;
+    }
+cout << "\nThank you for using the shape builder lab!" << endl << endl;
 }
