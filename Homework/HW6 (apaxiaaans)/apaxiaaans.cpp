b2f27f015a56cc582d9f5f40bb9272daff9f9649
AndrewSpencer23 <alspencer@mavs.coloradomesa.edu>
2023-11-27 09:44:56 -0800

    HW6 progress

--- Homework/HW6 (apaxiaaans)/apaxiaaans.cpp
+++ Homework/HW6 (apaxiaaans)/apaxiaaans.cpp
@@ -2,9 +2,31 @@
 // Andrew Spencer
 
 #include <iostream>
-#include <cstdio>
+#include <string>
+#include <sstream>
 
-int main (int argc, char* argv[])  {
-    
+using namespace std;
+
+string getString();
+double splitString(string&);
+
+int main(int argc, char* argv[])  {
+    string apaxPhrase;
+    string finalPhrase;
+    apaxPhrase = getString();
+    finalPhrase = splitString(apaxPhrase);
+    string words[apaxPhrase.length()];
     return 0;
+}
+
+string getString()  {
+    string phrase;
+    cout << "Please enter an apaxiaaan phrase in all lowercase between 1 and 250 characters: ";
+    cin >> phrase;
+    return phrase;
+}
+
+double splitString(string&)  {
+    string splitPhrase;
+    return splitPhrase;
 }
