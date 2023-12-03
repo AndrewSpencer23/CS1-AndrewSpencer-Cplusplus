5c2c4956b283300db9d40daee74b30ac7b74de3f
AndrewSpencer23 <alspencer@mavs.coloradomesa.edu>
2023-12-02 20:00:57 -0800

    HW7 Progress

--- Homework/HW7/HW7.cpp
+++ Homework/HW7/HW7.cpp
@@ -15,10 +15,51 @@
 
 using namespace std;
 
-// Function prototypes
-void sortPiecesArray(int, int[], int);
-int calcPieces(int, int[], int, int);
-void test();
+// I removed my function prototypes and moved my main function.
+// Probably not a great idea but I was getting confused about pass by in my prototypes.
+
+// Instead of using two seperate functions to sort the array for each Bob and Alice,
+// I used one void function that utilizes boolean in order to sort the array.
+void sortPiecesArray(int numPieces, int pieces[], int tmpNum) {
+    bool sort = false;
+    if (numPieces <= 15 && numPieces >= 1) {
+        for(int i = 0; i < numPieces - 1; i++) {
+            sort = true;
+            for(int j = 0; j < (numPieces - i) -1; j++) {
+                if(pieces[j] <= pieces[j+1]) {
+                    tmpNum = pieces[j];
+                    pieces[j] = pieces[j+1];
+                    pieces[j+1] = tmpNum;
+                    sort = false;
+                }
+            }
+            if (sort)
+                break;
+        }
+    }
+}
+
+// I got rid of my two int functions to calculate each of their values and instead used one void function that
+// Passes in existing variables to execute the calculation (Muuuuuch easier than what I was trying to do.)
+void calcPieces(int numPieces, int pieces[], int &bobCalc, int &aliceCalc) {
+    // Function to calculate Alice's value
+    if (numPieces <= 15 && numPieces >= 1) {
+        for(int i = 0; i < numPieces; i++) {
+            if (i%2 == 0) {
+                // If the value i in the array is even, add it to Alice's total.
+                aliceCalc = aliceCalc + pieces[i];
+            }
+            else {
+                // If i is odd, add it to Bob's total.
+                bobCalc = bobCalc + pieces[i];
+            }
+        }
+    }
+}
+
+void test() {
+    // Test function
+}
 
 int main(int argc, char* argv[]) {
     // Main function
@@ -51,46 +92,4 @@ int main(int argc, char* argv[]) {
     return 0;
 }
 
-// Instead of using two seperate functions to sort the array for each Bob and Alice,
-// I used one void function that utilizes boolean in order to sort the array.
-void sortPiecesArray(int numPieces, int pieces[], int tmpNum) {
-    bool sort = false;
-    if (numPieces <= 15 && numPieces >= 1) {
-        for(int i = 0; i < numPieces - 1; i++) {
-            sort = true;
-            for(int j = 0; j < (numPieces - i) -1; j++) {
-                if(pieces[j] <= pieces[j+1]) {
-                    tmpNum = pieces[j];
-                    pieces[j] = pieces[j+1];
-                    pieces[j+1] = tmpNum;
-                    sort = false;
-                }
-            }
-            if (sort)
-                break;
-        }
-    }
-}
 
-// I got rid of my two int functions to calculate each of their values and instead used one void function that
-// Passes in existing variables to execute the calculation (Muuuuuch easier than what I was trying to do.)
-void calcPieces(int numPieces, int *pieces[], int &bobCalc, int &aliceCalc) {
-    // Function to calculate Alice's value
-    if (numPieces <= 15 && numPieces >= 1) {
-        for(int i = 0; i < numPieces; i++) {
-            if (i%2 == 0) {
-                // If the value i in the array is even, add it to Alice's total.
-                aliceCalc = aliceCalc + *pieces[i];
-            }
-            else {
-                // If i is odd, add it to Bob's total.
-                bobCalc = bobCalc + *pieces[i];
-            }
-        }
-    }
-}
-
-
-void test() {
-    // Test function
-}
