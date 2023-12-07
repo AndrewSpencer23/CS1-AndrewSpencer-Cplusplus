dd9c6bd60e5a59114beef5a87dcd4c240fc4c7d6
AndrewSpencer23 <alspencer@mavs.coloradomesa.edu>
2023-12-07 15:35:39 -0800

    final progress commit

--- Homework/Final Project/final.cpp
+++ Homework/Final Project/final.cpp
@@ -9,8 +9,19 @@
 
 using namespace std;
 
+void printBoard(char [][3]);
+
 int main(int argc, char *argv[]) {
-    char board[3][3];
+    char board[3][3] = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+
+    printBoard(board);
+    
+    return 0;
+}
+
+void printBoard(char board[3][3]) {
+    cout << setfill('-') << setw(7) << "" << endl;
+    cout << setfill(' ');
 
     for(size_t i = 0; i < 3; i++) {
         cout << "|";
@@ -21,5 +32,4 @@ int main(int argc, char *argv[]) {
         cout << setfill('-') << setw(7) << "" << endl;
         cout << setfill(' ');
     }
-    return 0;
 }
