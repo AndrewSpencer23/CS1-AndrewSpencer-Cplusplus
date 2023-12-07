45c2ea5d6944c8ba60b85beecc5a6fdf66f03535
AndrewSpencer23 <alspencer@mavs.coloradomesa.edu>
2023-12-07 15:59:44 -0800

    HW8 progress

--- Homework/HW8/HW8.cpp
+++ Homework/HW8/HW8.cpp
@@ -12,19 +12,43 @@
 
 using namespace std;
 
+struct person {
+    string fName;
+    string lName;
+    int score1;
+    int score2;
+    int score3;
+    int score4;
+};
+
+
 int countLines(fstream&);
-void readVals(fstream&, int);
-double calcAvg();
+void readVals(vector<person>, int);
+float calcAvg(person);
 void writeVals();
 
 int main(int argc, char* argv[]) {
     int numLines = 0;
+    vector<person> number;
+    string inFileName, outFileName;
+    cout << "Please enter an input file name: ";
+    getline(cin, inFileName);
+    
+    cout << "\n\n Please enter an output file name: ";
+    getline(cin, inFileName);
+
+    
     fstream fs;
-    fs.open("csci112in.txt", ios_base::in);
+    fs.open(inFileName, ios_base::in);
+
     numLines = countLines(fs);
 
     fs.close();
     return 0;
+}
+
+void readVals(vector<person>, int) {
+
 }
 
 int countLines(fstream& fs) {
