59e1193442af749c33b77d2d4e367407b518b00a
AndrewSpencer23 <alspencer@mavs.coloradomesa.edu>
2023-12-07 15:18:42 -0800

    final first commit

--- Homework/Final Project/final.cpp
+++ Homework/Final Project/final.cpp
@@ -3,3 +3,23 @@
 // Final Project
 
 // Tick-Tack-Toe Game
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    char board[3][3];
+
+    for(size_t i = 0; i < 3; i++) {
+        cout << "|";
+        for(size_t j = 0; j < 3; j++) {
+            cout << board[i][j] << "|";
+        }
+        cout << endl;
+        cout << setfill('-') << setw(7) << "" << endl;
+        cout << setfill(' ');
+    }
+    return 0;
+}
