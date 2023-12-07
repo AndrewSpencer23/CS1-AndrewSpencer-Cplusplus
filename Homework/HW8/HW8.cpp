c29008e5f119e0633aaf1f81e01c961b043a6b51
AndrewSpencer23 <alspencer@mavs.coloradomesa.edu>
2023-12-07 14:44:23 -0800

    HW8 First Commit

--- Homework/HW8/HW8.cpp
+++ Homework/HW8/HW8.cpp
@@ -2,11 +2,29 @@
 // Andrew Spencer
 
 #include <iostream>
-#include <vector>
 #include <string>
+#include <fstream>
+#include <sstream>
+#include "csci112in.txt"
+#include "csci112out.txt"
 
 using namespace std;
 
+int countLines(fstream&);
+void readVals(fstream&, int);
+double calcAvg();
+void writeVals();
+
 int main(int argc, char* argv[]) {
+    int numLines = 0;
+    fstream fs;
+    fs.open("csci112in.txt", ios_base::in);
+    numLines = countLines(fs);
+
+    fs.close();
     return 0;
+}
+
+int countLines(fstream& fs, int numLines) {
+    return numLines;
 }
