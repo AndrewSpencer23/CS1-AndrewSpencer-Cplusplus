42c9fa0674846cbf55788654595a31b28c175d46
AndrewSpencer23 <alspencer@mavs.coloradomesa.edu>
2023-12-07 16:18:16 -0800

    HW8 progress

--- Homework/HW8/HW8.cpp
+++ Homework/HW8/HW8.cpp
@@ -21,36 +21,58 @@ struct person {
     int score4;
 };
 
-
-int countLines(fstream&);
-void readVals(vector<person>, int);
 float calcAvg(person);
-void writeVals();
+void readVals(vector<person>, string);
+void writeVals(vector<person>, string);
 
 int main(int argc, char* argv[]) {
+
     int numLines = 0;
     vector<person> number;
     string inFileName, outFileName;
+
     cout << "Please enter an input file name: ";
     getline(cin, inFileName);
+    readVals(number, inFileName);
     
     cout << "\n\n Please enter an output file name: ";
-    getline(cin, inFileName);
+    getline(cin, outFileName);
+    writeVals(number, outFileName);
 
-    
-    fstream fs;
-    fs.open(inFileName, ios_base::in);
-
-    numLines = countLines(fs);
-
-    fs.close();
     return 0;
 }
 
-void readVals(vector<person>, int) {
+void readVals(vector<person> &testScores, string inFileName) {
+    fstream ifs;
+    ifs.open(inFileName, ios_base::in);
+    while(!ifs.eof()) {
+        string tempString;
+        person student;
+        getline(ifs, tempString);
+        stringstream iss(tempString);
+        for(size_t i = 0; i < 6; i++) {
+            string testString;
+            iss >> testString;
 
-}
+            if(i == 0) {
+                student.fName = testString;
+            }
+            else if(i == 1) {
+                student.lName = testString;
+            }
+            else if(i == 2) {
+                student.score1 = stoi(testString);
+            }
+            else if(i == 3) {
+                student.score2 = stoi(testString);
+            }
+            else if(i == 4) {
+                student.score3 = stoi(testString);
+            }
+            else if(i == 5) {
+                student.score4 = stoi(testString);
+            }
+        }
 
-int countLines(fstream& fs) {
-    return 0;
+    }
 }
